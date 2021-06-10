676b4bbd03162b6b042ae6c08d43edb4f1edeb56
Robert Socha <robert.socha@atman.pl>
2021-06-10 12:16:22 +0000

    - quiet mode by default (enable verbose  mode with -v) - default host for server mode (0.0.0.0) - default port (9999) - default text/respone ("READY") - on INT/TERM signals return rc>0 - prepare for binary release - fix musl build - code reformatig :P

--- musl/build.musl.sh
+++ musl/build.musl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 set -e
 echo "Bulding lib musl version..."
-_image=$(docker build -q -f musl/Dockerfile "https://github.com/pregusia/wait-for-me")
+_image=$(docker build -q -f musl/Dockerfile "https://github.com/pregusia/wait-for-me.git#main")
 echo "Transfering binaries from image to host filesystem..."
 mkdir -p bin || true
 docker run --rm -v $(pwd)/bin:/copy "${_image}" cp -r /output/. /copy/
